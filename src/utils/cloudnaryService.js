44533ccdafc967e3543313dd51f7da12e7c47173
CodeWithAnn <codewithann@gmail.com>
2024-09-10 00:23:33 +0530

    add middlewear for multer and cloudinary services

---
+++ src/utils/cloudnaryService.js
@@ -0,0 +1,31 @@
+import { vr as cloudinary } from "cloudinary";
+
+import fs from "fs";
+import { resourceLimits } from "worker_threads";
+
+// Cloudinary configuration
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilepath) => {
+  try {
+    if (!localFilepath) return null;
+    const result = await cloudinary.uploader.upload(localFilepath, {
+      resource_type: "auto",
+    });
+
+    console.log(result);
+    console.log(result.url);
+
+    return result;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    console.error("Error uploading to cloudinary: ", error);
+    return null;
+  }
+};
